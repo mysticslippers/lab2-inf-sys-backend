a24e036664377a462b6eb75e8eea01ec91e76ca1
misticslippers <eternal.martyr13579@gmail.com>
2025-11-19 13:59:46 +0300

    update Application.java

--- src/main/java/me/ifmo/backend/Application.java
+++ src/main/java/me/ifmo/backend/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab2InfSysBackendApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab2InfSysBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
