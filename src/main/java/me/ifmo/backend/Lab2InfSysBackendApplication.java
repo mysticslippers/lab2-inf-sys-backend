b0339d209be1ebbf5cd8839a664a1b12637de47e
misticslippers <eternal.martyr13579@gmail.com>
2025-11-19 10:44:32 +0300

    Initial commit

---
+++ src/main/java/me/ifmo/backend/Lab2InfSysBackendApplication.java
@@ -0,0 +1,13 @@
+package me.ifmo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2InfSysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2InfSysBackendApplication.class, args);
+	}
+
+}
