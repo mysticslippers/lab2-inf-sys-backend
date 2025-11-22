a7a02ec3ffc2483d7e804b16e282498112cc8587
misticslippers <eternal.martyr13579@gmail.com>
2025-11-22 13:13:03 +0300

    add ImportOperationDTO

---
+++ src/main/java/me/ifmo/backend/DTO/ImportOperationDTO.java
@@ -0,0 +1,22 @@
+package me.ifmo.backend.DTO;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ImportOperationDTO {
+
+    private Long id;
+    private String username;
+    private String objectType;
+    private String status;
+    private Integer importedCount;
+    private String errorMessage;
+    private LocalDateTime startedAt;
+    private LocalDateTime finishedAt;
+}
