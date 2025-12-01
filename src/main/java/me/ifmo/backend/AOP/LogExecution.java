9d215148ab964bdeda073155b9721725969ab584
misticslippers <eternal.martyr13579@gmail.com>
2025-12-01 10:29:18 +0300

    add LogExecution

---
+++ src/main/java/me/ifmo/backend/AOP/LogExecution.java
@@ -0,0 +1,12 @@
+package me.ifmo.backend.AOP;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LogExecution {
+    boolean logArgs() default true;
+
+    boolean logResult() default true;
+}
