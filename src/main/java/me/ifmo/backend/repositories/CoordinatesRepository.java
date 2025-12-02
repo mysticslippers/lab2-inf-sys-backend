e25b9564a35a9c189b1e76ff4316745d2d6f4dd6
misticslippers <eternal.martyr13579@gmail.com>
2025-12-02 19:04:37 +0300

    fix CoordinatesRepository

--- src/main/java/me/ifmo/backend/repositories/CoordinatesRepository.java
+++ src/main/java/me/ifmo/backend/repositories/CoordinatesRepository.java
@@ -4,6 +4,12 @@ import me.ifmo.backend.entities.Coordinates;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CoordinatesRepository extends JpaRepository<Coordinates, Long> {
+
+    boolean existsByXAndY(Double x, Float y);
+
+    List<Coordinates> findAllByXAndY(Double x, Float y);
 }
