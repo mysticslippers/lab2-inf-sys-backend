8ed861f0b6bae562fe07eb4f17dd89861a992c33
misticslippers <eternal.martyr13579@gmail.com>
2026-02-05 13:51:43 +0300

    update Application.java

--- src/main/java/me/ifmo/backend/Application.java
+++ src/main/java/me/ifmo/backend/Application.java
@@ -1,9 +1,12 @@
 package me.ifmo.backend;
 
+import me.ifmo.backend.config.PasswordResetProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PasswordResetProperties.class)
 public class Application {
 
     public static void main(String[] args) {
